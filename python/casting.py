3973eb5b0d59fdebf009f6600739bcbbbcf00f42
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2024-01-18 14:42:41 +0530

    Casting in python

---
+++ python/casting.py
@@ -0,0 +1,11 @@
+""" 
+Casting In Python 
+   It can be achived by using constructor functions like int(), float(), str() etc.
+"""
+x = float(500);
+y = str(100)
+z = int(1.888)
+w = str("Himanshu")
+
+print(x, y, z, w)
+print(type(x), type(y), type(z), type(w))
